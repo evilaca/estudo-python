603eabadcfd474ab612f40415b657f3a043a6dd9
evilaca <115052003+evilaca@users.noreply.github.com>
2022-11-03 21:37:20 -0300

    estudo-python

---
+++ fernando-amaral/python-inic/exer01.py
@@ -0,0 +1,25 @@
+x = 10
+y = 20
+z = 100
+w = (x+y)*z/100
+print(w)
+print("Este texto será impresso no console")
+print(x)
+print("Texto e duas variáveis: ",x,",",z)
+
+m="Python"
+print(type(m))
+print(type(x))
+
+var=input("Informe o Valor: ")
+var=int(var)
+print(type(var))
+var=float(var)
+print(type(var))
+
+#este código é um comentário
+x=10 #este código é um comentário
+
+''' Aqui come~ça um comentário
+aqui o comentário continua
+e aqui ele termina'''
---
+++ fernando-amaral/python-inic/exer02.py
@@ -0,0 +1,14 @@
+lst=[1,2,3,4,5]
+print(lst)
+lst2 = [1,2,3,"4",True]
+print (lst2)
+lst3 = [12,lst2,"a"]
+print (lst3)
+
+lst4=list(range(1,11))
+print(lst4)
+
+
+print(len(lst))
+
+
